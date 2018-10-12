3abe74ca5ce017da38c55f09a080f069eb9f1a2c
mfsoftworks <it@nygmarosebeauty.com>
2018-10-12 10:33:35 +0800

    [ui] added info boxes with dynamic js for dashboard

--- dist/js/monitoring-get-system-info.js
+++ dist/js/monitoring-get-system-info.js
@@ -4,47 +4,70 @@
  * @Author MF Softworks <mf@nygmarosebeauty.com>
  */
 
-function getSystemInfo() {
+var systemJSON;
+var dataJSON;
 
+// First run
+getDevices();
+getUniqueDevices();
+
+// Set 60 second interval
+var getDevicesInterval = setInterval(
+    function() { 
+        getDevices() 
+    }, 
+    60 * 1000
+);
+
+var deviceCountInterval = setInterval(
+    function() { 
+        getUniqueDevices() 
+    }, 
+    60 * 1000
+);
+
+function getDevices() {
     $.get(
         "../../api/get.php",
 
         function(response) {
-            var systemJSON = JSON.parse(response);
-            for(var i=0; i<10; i++) {
-                device = systemJSON.rows[i];
-                console.log( device );
-            }
+
+            systemJSON = JSON.parse(response);
+            dataJSON = systemJSON.rows;
+
+            // Check device info
+            //for(var i=0; i<dataJSON.length; i++) {
+            //    console.log(dataJSON[i]);
+            //}
+
+            $(document).ready(function() {
+                systemsAverageCPUPercent();
+                systemsAverageMemoryPercent();
+                systemsAverageUptime();
+            });
         }
     );
-
 };
 
 function getUniqueDevices() {
-
     $.get(
         "../../api/get.php",
         {
             condition: "unique devices"
         },
         function(response) {
-            $("#dashboard-device-count").append(response);
+            $(document).ready(function() {
+                $("#dashboard-device-count").empty();
+                $("#dashboard-device-count").append(response);
+            });
         }
     );
-
 };
 
 function systemsAverageCPUPercent() {
-    $.get(
-        "../../api/get.php",
-
-        function(response) {
-            var dataJSON = JSON.parse(response);
-            var systemsJSON = dataJSON.rows;
-            var cpuAvg = calcAvgCPU(systemsJSON);
-            $('#dashboard-cpu-average').append(cpuAvg + "<small>%</small>");
-        }
-    );
+    var cpuAvg = calcAvgCPU(dataJSON);
+    $('#dashboard-cpu-average').empty();
+    $('#dashboard-cpu-average').append(cpuAvg + "<small>%</small>");
 };
 function calcAvgCPU(dataJSON) {
     var cpuCount = 0.0;
@@ -57,16 +80,9 @@ function calcAvgCPU(dataJSON) {
 };
 
 function systemsAverageMemoryPercent() {
-    $.get(
-        "../../api/get.php",
-
-        function(response) {
-            var dataJSON = JSON.parse(response);
-            var systemsJSON = dataJSON.rows;
-            var memAvg = calcAvgMemory(systemsJSON);
-            $('#dashboard-memory-average').append(memAvg + "<small>%</small>");
-        }
-    );
+    var memAvg = calcAvgMemory(dataJSON);
+    $('#dashboard-memory-average').empty();
+    $('#dashboard-memory-average').append(memAvg + "<small>%</small>");
 };
 function calcAvgMemory(dataJSON) {
     var memCount = 0.0;
@@ -76,4 +92,31 @@ function calcAvgMemory(dataJSON) {
     }
     memAvg = memCount / i;
     return memAvg.toFixed(2);
+};
+
+function systemsAverageUptime() {
+    var uptimeAvg = calcAvgUptime(dataJSON);
+    $("#dashboard-uptime-average").empty();
+    $("#dashboard-uptime-average").append(uptimeAvg);
+};
+function calcAvgUptime(dataJSON) {
+    var uptimeCount = 0;
+    var uptimeAvg;
+    for(var i = 0; i < dataJSON.length; i++) {
+        uptimeCount += parseInt(dataJSON[i].uptime);
+    }
+    uptimeAvg = uptimeCount / i;
+    
+    var hours = Math.floor(uptimeAvg / 3600);
+    var minutes = Math.floor((uptimeAvg % 3600) / 60);
+
+    if(hours > 0) {
+        return hours + " hours " + minutes + " mins";
+    }
+    else if(minutes > 0) {
+        return minutes + " mins";
+    }
+    else {
+        return uptimeAvg + " seconds";
+    }
 };
